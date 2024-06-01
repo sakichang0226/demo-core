1eecd7e7ad388fe213fa40685be3ac7e8c95326d
sakichang0226 <saki.develop0226@gmail.com>
2024-06-01 12:33:05 +0900

    feat: 例外ハンドリングを共通化させる

--- demo-s3/src/main/java/com/demo/demo_s3/Service/S3Service.java
+++ demo-s3/src/main/java/com/demo/demo_s3/Service/S3Service.java
@@ -1,32 +1,18 @@
 package com.demo.demo_s3.Service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.demo.demo_s3.client.DemoS3Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.IOException;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
 
-    private final AmazonS3 s3Client;
+    private final DemoS3Client client ;
 
-    public Map<String, String> getYamlFile() throws IOException {
-        GetObjectRequest request = new GetObjectRequest("spring-testbuket-2024", "test.yaml");
-        S3Object object = s3Client.getObject(request);
-
-        try (S3ObjectInputStream reader = object.getObjectContent()) {
-            Yaml yaml = new Yaml();
-            Map<String,String> response =  (Map<String, String>) yaml.load(reader);
-
-            return response;
-        }
+    public Map<String, String> getYamlFile() {
+        return client.getObjectFromYaml("spring-testbuket-2024","test");
     }
 }
---
+++ demo-s3/src/main/java/com/demo/demo_s3/client/DemoS3Client.java
@@ -0,0 +1,75 @@
+package com.demo.demo_s3.client;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+
+
+/**
+ * S3からファイルを取得し、オブジェクトに変換するためのクライアントクラス
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DemoS3Client {
+
+    private final AmazonS3 s3Client;
+
+    /**
+     * S3のバケットからオブジェクトを取得し、指定したクラスオブジェクトに変換する
+     * @param bucketName バケット名
+     * @param key　ファイル名
+     * @return 変換したいオブジェクトクラス
+     */
+    public <T> T getObjectFromYaml(String bucketName, String key) {
+
+        try {
+            GetObjectRequest request = new GetObjectRequest(bucketName, key + ".yaml");
+            S3Object object = s3Client.getObject(request);
+
+            S3ObjectInputStream reader = object.getObjectContent();
+            Yaml yaml = new Yaml();
+
+            return (T) yaml.load(reader);
+
+        } catch (AmazonS3Exception e) {
+            log.warn(e.getMessage());
+        }
+
+        return null;
+    }
+
+    /**
+     * S3のバケットからオブジェクトを取得し、指定したクラスオブジェクトに変換する
+     * @param bucketName バケット名
+     * @param key　ファイル名
+     * @return 変換したいオブジェクトクラス
+     */
+    public <T> T getObjectFromFile(String bucketName, String key, Class<T> clazz) {
+
+        try {
+            GetObjectRequest request = new GetObjectRequest(bucketName, key + ".json");
+            S3Object object = s3Client.getObject(request);
+
+            S3ObjectInputStream reader = object.getObjectContent();
+            ObjectMapper mapper = new ObjectMapper();
+
+            return mapper.readValue(reader, clazz);
+        } catch (AmazonS3Exception | IOException e) {
+            log.warn(e.getMessage());
+        }
+
+        return null;
+    }
+
+}
