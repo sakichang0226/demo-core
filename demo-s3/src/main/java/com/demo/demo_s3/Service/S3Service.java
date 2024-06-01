0aef76c4c288425b2f016a896cef2cf23749f87d
sakichang0226 <saki.develop0226@gmail.com>
2024-06-01 13:25:30 +0900

    feat: バケット名を定数管理する

--- demo-s3/src/main/java/com/demo/demo_s3/Service/S3Service.java
+++ demo-s3/src/main/java/com/demo/demo_s3/Service/S3Service.java
@@ -1,6 +1,7 @@
 package com.demo.demo_s3.Service;
 
 import com.demo.demo_s3.client.DemoS3Client;
+import com.demo.demo_s3.constant.S3BucketEndPoint;
 import com.demo.demo_s3.entity.Test;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,10 +15,10 @@ public class S3Service {
     private final DemoS3Client client ;
 
     public Map<String, String> getYamlFile() {
-        return client.getObjectFromYaml("spring-testbuket-2024","test");
+        return client.getObjectFromYaml(S3BucketEndPoint.TEST,"test");
     }
 
     public Test getTest() {
-        return client.getObjectFromFile("spring-testbuket-2024", "test2", Test.class);
+        return client.getObjectFromFile(S3BucketEndPoint.TEST, "test2", Test.class);
     }
 }
