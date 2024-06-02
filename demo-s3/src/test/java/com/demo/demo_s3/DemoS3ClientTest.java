300958dc99d495250323a7e1cf9a8fd1d1c03151
sakichang0226 <saki.develop0226@gmail.com>
2024-06-02 13:54:27 +0900

    feat: S3読み込み失敗時のJUnitテストを記載

---
+++ demo-s3/src/test/java/com/demo/demo_s3/DemoS3ClientTest.java
@@ -0,0 +1,44 @@
+package com.demo.demo_s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.S3Object;
+import com.demo.demo_s3.client.DemoS3Client;
+import com.demo.demo_s3.constant.S3BucketEndPoint;
+import com.demo.demo_s3.exception.DemoS3BusinessException;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class DemoS3ClientTest {
+
+    AmazonS3 s3 = mock(AmazonS3.class);
+
+    @InjectMocks
+    DemoS3Client client = new DemoS3Client(s3);
+
+    @Test
+    public void test_getObjectFromYaml_S3のファイルが見つからないとき適切な例外クラスが返されるか() {
+        when(s3.getObject(any())).thenThrow(AmazonS3Exception.class);
+
+        assertThrows(DemoS3BusinessException.class, () -> client.getObjectFromYaml(S3BucketEndPoint.TEST, "test"));
+    }
+
+    @Test
+    public void test_getObjectFromFile_S3のファイルが見つからないとき適切な例外クラスが返されるか() {
+        when(s3.getObject(any())).thenThrow(AmazonS3Exception.class);
+
+        assertThrows(DemoS3BusinessException.class, () -> client.getObjectFromFile(S3BucketEndPoint.TEST, "test", Test.class));
+    }
+
+    @Test
+    public void test_getObjectFromFile_list_S3のファイルが見つからないとき適切な例外クラスが返されるか() {
+        when(s3.getObject(any())).thenThrow(AmazonS3Exception.class);
+
+        assertThrows(DemoS3BusinessException.class, () -> client.getObjectFromFile(S3BucketEndPoint.TEST, "test"));
+    }
+
+
+}
