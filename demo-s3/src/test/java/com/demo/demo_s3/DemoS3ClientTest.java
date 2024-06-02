322f358e97e5f9a204bae55b8485194584531eb4
sakichang0226 <saki.develop0226@gmail.com>
2024-06-02 18:22:59 +0900

    feat: JSONパースエラー発生時の例外テストを追加

--- demo-s3/src/test/java/com/demo/demo_s3/DemoS3ClientTest.java
+++ demo-s3/src/test/java/com/demo/demo_s3/DemoS3ClientTest.java
@@ -3,11 +3,16 @@ package com.demo.demo_s3;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.S3Object;
+
 import com.demo.demo_s3.client.DemoS3Client;
 import com.demo.demo_s3.constant.S3BucketEndPoint;
 import com.demo.demo_s3.exception.DemoS3BusinessException;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -30,7 +35,7 @@ public class DemoS3ClientTest {
     public void test_getObjectFromFile_S3のファイルが見つからないとき適切な例外クラスが返されるか() {
         when(s3.getObject(any())).thenThrow(AmazonS3Exception.class);
 
-        assertThrows(DemoS3BusinessException.class, () -> client.getObjectFromFile(S3BucketEndPoint.TEST, "test", Test.class));
+        assertThrows(DemoS3BusinessException.class, () -> client.getObjectFromFile(S3BucketEndPoint.TEST, "test", com.demo.demo_s3.entity.Test.class));
     }
 
     @Test
@@ -40,5 +45,26 @@ public class DemoS3ClientTest {
         assertThrows(DemoS3BusinessException.class, () -> client.getObjectFromFile(S3BucketEndPoint.TEST, "test"));
     }
 
+    @Test
+    public void test_getObjectFromFile_対象ファイルのパース処理失敗時適切な例外を返すか() throws FileNotFoundException {
+        InputStream file = new FileInputStream("src/test/resources/test2.json");
+        S3Object object = new S3Object();
+        object.setObjectContent(file);
+
+        when(s3.getObject(any())).thenReturn(object);
+
+        assertThrows(DemoS3BusinessException.class, () -> client.getObjectFromFile(S3BucketEndPoint.TEST, "test2", com.demo.demo_s3.entity.Test.class));
+    }
+
+    @Test
+    public void test_getObjectFromFile_list_対象ファイルのパース処理失敗時適切な例外を返すか() throws FileNotFoundException {
+        InputStream file = new FileInputStream("src/test/resources/test3.json");
+        S3Object object = new S3Object();
+        object.setObjectContent(file);
+
+        when(s3.getObject(any())).thenReturn(object);
+
+        assertThrows(DemoS3BusinessException.class, () -> client.getObjectFromFile(S3BucketEndPoint.TEST, "test3"));
+    }
 
 }
