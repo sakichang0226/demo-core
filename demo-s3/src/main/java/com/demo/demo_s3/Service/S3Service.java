68077935faf586fd5fc7afc6c1dfb31d65f760b0
sakichang0226 <saki.develop0226@gmail.com>
2024-06-02 14:57:34 +0900

    fix: Service層のJavadocコメントを追加

--- demo-s3/src/main/java/com/demo/demo_s3/Service/S3Service.java
+++ demo-s3/src/main/java/com/demo/demo_s3/Service/S3Service.java
@@ -9,20 +9,36 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * S3のクライアントを呼び出し、オブジェクトに変換するためのサービス層<br>
+ * 親プロジェクト側からサービス層を呼び出し処理をする
+ */
 @Service
 @RequiredArgsConstructor
 public class S3Service {
 
     private final DemoS3Client client ;
 
+    /**
+     * Yamlファイルからオブジェクトにパースする
+     * @return yamlのキー,バリューの組み合わせ
+     */
     public Map<String, String> getYamlFile() {
         return client.getObjectFromYaml(S3BucketEndPoint.TEST,"test");
     }
 
+    /**
+     * S3のJSONファイルから、単体のオブジェクトにパースする
+     * @return Testクラス
+     */
     public Test getTest() {
         return client.getObjectFromFile(S3BucketEndPoint.TEST, "test2", Test.class);
     }
 
+    /**
+     * S3のJSONファイルを取得し、複数のTestオブジェクトの項目を返す
+     * @return Testクラスのリスト
+     */
     public List<Test> getTestList() {
         return client.getObjectFromFile(S3BucketEndPoint.TEST, "test3");
     }
